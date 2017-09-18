889cfcfd47e8f0326431ec6ee5fbc2b0e5e2ef03
Gordon Quad <gordon@niflheim.info>
2017-09-18 23:17:52 +0000

    title limited and convertion of encoding added

--- src/BoTox/Bots/TitleBot.hs
+++ src/BoTox/Bots/TitleBot.hs
@@ -5,22 +5,27 @@ import           BoTox.Types
 import           BoTox.Utils
 
 import           Control.Auto
+import           Control.Monad
 import           Control.Auto.Blip
 import           Control.Auto.Effects (arrMB)
 import           Control.Exception (try)
-import           Control.Lens
 import           Control.Monad.IO.Class (MonadIO, liftIO)
+import qualified Codec.Text.IConv as IConv
 import qualified Data.ByteString.Lazy.Char8 as LB
+import qualified Data.ByteString as BS
 import qualified Data.List as L
 import           Data.Maybe
 import qualified Data.String as S
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
-import           Network.HTTP.Client (HttpException(..))
-import           Network.Wreq
+import qualified Data.Conduit.Binary as CB
+import           Data.Conduit
+import           Network.HTTP.Simple
+import           Network.HTTP.Types.Header
 import Prelude hiding           ((.), id)
 import           Text.HTML.TagSoup
 import           Text.HTML.TagSoup.Match
+import           Text.Regex
 
 
 titleBot :: MonadIO m => GroupBot m
@@ -66,23 +71,39 @@ extractTitle :: String -- | URL
 extractTitle url = do
   eitherWebPage <- getWebPage url  -- webpage :: L.ByteString
   case eitherWebPage of
-    Right webpage -> do
+    Right (contentType, webpage) -> do
       let errorText    = "Webpage has no title."
-          parsed       =  parseTags webpage
+          parsed       =  parseTags $ webpage
           titleTags    =  takeWhile (not . tagCloseNameLit "title") $ dropWhile (not . tagOpenNameLit "title") parsed
           result       =  mconcat $ catMaybes $ map maybeTagText titleTags
       case result of 
         "" ->  return errorText
-        r ->  return . mapError . TE.decodeUtf8' $ LB.toStrict r -- To do: handle web pages that don't use UTF8
+        r -> return $ mapError . TE.decodeUtf8' . LB.toStrict $ toCharset (decodeCharset contentType) r -- To do: handle web pages that don't use UTF8
     Left ex -> return $ T.pack $ "Fetching page title failed due to " ++ ex
   where
     mapError = either (S.fromString . show) id
 
 
 getWebPage :: String -- | Url
-            -> IO (Either String LB.ByteString) -- | Either "HttpError" Webpage
+            -> IO (Either String (Maybe BS.ByteString, LB.ByteString)) -- | Either "HttpError" Webpage
 getWebPage url = do
-  eResponse <- try (get url) :: IO (Either HttpException (Response LB.ByteString))
+  eResponse <- try (getUrl url) :: IO (Either HttpException (Maybe BS.ByteString, LB.ByteString))
   case eResponse of
-    Right response -> return $ Right $ response ^. responseBody
+    Right (contentType, response) -> return $ Right $ (contentType, response)
     Left ex -> return $ Left $ "HTTP Exception: " ++ show ex
+
+getUrl :: String -> IO (Maybe BS.ByteString, LB.ByteString)
+getUrl url = do
+  req <- parseRequest url
+  withResponse req (\resp -> liftM ((,) (listToMaybe $ getResponseHeader hContentType resp)) $ (getResponseBody resp $$ CB.take 10240))
+
+decodeCharset :: Maybe BS.ByteString -> String
+decodeCharset Nothing = "UTF-8"
+decodeCharset (Just c) = (either (\_ -> "UTF-8") (matchCharset . T.unpack) $ TE.decodeUtf8' c)
+
+matchCharset :: String -> String
+matchCharset t = maybe "UTF-8" head $ matchRegex (mkRegex "charset=([a-zA-Z0-9-]+)") t
+
+toCharset :: String -> LB.ByteString -> LB.ByteString
+toCharset enc title = IConv.convert enc "UTF-8" title
+
